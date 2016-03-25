50e5d52e82dac0eb2729089244f0ba36da18803a
Xavi Nguyen <vphat28@gmail.com>
2016-03-25 11:09:18 +0700

    Add plugin main file

---
+++ ecorepay-woocommerce.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Plugin Name: Ecorepay Woocommerce
+* Plugin URI: https://sundaysea.com/
+* Description: Add Ecorepay payment method to WooCommerce.
+* Version: 1.0
+* Author: Xavi Nguyen
+* Author URI:
+* License GPL2
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
